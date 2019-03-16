a176dc9ca140837be278229c84f93aa469a5b67f
Fadee kannah <fadeekannah@gmail.com>
2019-03-15 20:24:24 -0700

    Remove caching/ offline support

--- static/490/service-worker.js
+++ static/490/service-worker.js
@@ -1,4 +1,4 @@
-var CACHE = 'v2.1';
+var CACHE = 'v2.2';
 const CACHE_MISS = 'cache-miss';
 var filesToCache = [
   '/490/',
@@ -11,7 +11,6 @@ var filesToCache = [
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   self.skipWaiting();
-  e.waitUntil(precache());
 });
 
 self.addEventListener('activate', function(e) {
@@ -28,40 +27,3 @@ self.addEventListener('activate', function(e) {
   );
   return self.clients.claim();
 });
-
-
-self.addEventListener('fetch', function(e) {
-  console.log('[Service Worker] Fetch', e.request.url);
-  e.respondWith(fromCache(e.request).catch(() => fromNetworkAndCache(e.request)));
-});
-
-function precache() {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.addAll(filesToCache);
-  });
-}
-
-function fromNetworkAndCache(request) {
-  return fetch(request.clone()).then(function (response) {
-    if(!response || response.status !== 200) {
-      return response; // response stream consumed
-    }
-    let resClone = response.clone();
-    return caches.open(CACHE)
-      .then(function(cache) {
-        console.log('caching', request.url)
-        cache.put(request, resClone); // reqCopy, resCopy streams consumed
-        return response; // response stream consumed
-      });
-  })
-}
-
-
-function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      if (matching) return matching;
-      return Promise.reject(CACHE_MISS)
-    });
-  });
-}
