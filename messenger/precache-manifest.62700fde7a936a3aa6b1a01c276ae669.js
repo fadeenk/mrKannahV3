e948d4366b604b19b579c69a2b23fa45bf754223
Fadee Kannah <fadeekannah@gmail.com>
2020-01-16 20:30:27 -0800

    Updates

---
+++ messenger/precache-manifest.62700fde7a936a3aa6b1a01c276ae669.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf0e570e6bb55107b63a78a9d82936f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7d63c8059f726cd31d7",
+    "url": "/static/js/2.3201a560.chunk.js"
+  },
+  {
+    "revision": "6bafae5f846e880ad28961fb05bbbdf7",
+    "url": "/static/js/2.3201a560.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63f925cd4d100495c8da",
+    "url": "/static/js/main.cdfc70f2.chunk.js"
+  },
+  {
+    "revision": "e97230cfbaa24b3b1dba",
+    "url": "/static/js/runtime-main.b1eca7c2.js"
+  }
+]);
